9983390da7fdebb38314cd4dbf95e354ebc27008
Caio Moraes V Santos <caio.moraes@radixeng.com.br>
2023-04-27 19:50:17 -0300

    Creating and customize Input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
     return (
@@ -16,8 +17,13 @@ export function NewGroup() {
                     subtitle="Crie a turma para adicionar as pessoas"
                 />
 
+                <Input
+                    placeholder="Nome da turma"
+                />
+
                 <Button
                     title="Criar"
+                    style={{ marginTop: 20 }}
                 />
             </Content>
         </Container>
