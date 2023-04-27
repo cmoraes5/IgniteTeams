b6ae43ec28a1128367be2fd49243581bf52feb0a
Caio Moraes V Santos <caio.moraes@radixeng.com.br>
2023-04-27 11:26:04 -0300

    Adding ShowBackButton feature

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,6 @@
 import styled from "styled-components/native";
+import { Ionicons } from '@expo/vector-icons';
+
 
 export const Container = styled.View`
     width: 100%;
@@ -12,3 +14,13 @@ export const Logo = styled.Image`
     width: 46px;
     height: 55px;
 `;
+
+export const BackButton = styled.TouchableOpacity`
+    flex: 1;
+`
+
+export const BackIcon = styled(Ionicons).attrs(({ theme }) => ({
+    name: "md-chevron-back-sharp",
+    size: 32,
+    color: theme.COLORS.WHITE
+}))``;
